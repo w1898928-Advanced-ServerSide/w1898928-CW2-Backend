f78a8a259c4285622c79036c85f33cdacdfd8cd8
Ramindu Jayaweera <ramindujayaweera2000@gmail.com>
2025-05-12 03:46:28 +0530

    update backend

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 const app = express();
 // Enable CORS for all routes
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3002',
   credentials: true
 }));
 const PORT = process.env.PORT || 4000;
